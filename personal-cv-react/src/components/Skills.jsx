ed98de8a83f93e1d17fcc239f0fc875a73759dfe
codeswagger24 <rolvekraken26@gmail.com>
2026-02-26 21:00:20 +0800

    WSAT-M5: Migrated CV to React

--- personal-cv-react/src/components/Skills.jsx
+++ personal-cv-react/src/components/Skills.jsx
@@ -1,31 +1,44 @@
+import { useState } from "react";
+
 function Skills() {
-    return (
-<section className="card">
-      <button id="toggleSkills">Show/Hide Skills</button>
-      <section className="card" id="skillsSection">
-        <h2>Skills</h2>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-        </ul>
-        <ul>
-          <li>Web Development
-            <ul>
-              <li>Frontend
-                <ul>
-                  <li>HTML</li>
-                  <li>CSS</li>
-                  <li>JavaScript</li>
-                </ul>
-              </li>
-              <li>Backend</li>
-            </ul>
-          </li>
-        </ul>
-      </section>
+  const [showSkills, setShowSkills] = useState(true);
+
+  return (
+    <section className="card">
+      <button onClick={() => setShowSkills(!showSkills)}>
+        Show/Hide Skills
+      </button>
+
+      {showSkills && (
+        <section className="card">
+          <h2>Skills</h2>
+
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+          </ul>
+
+          <ul>
+            <li>
+              Web Development
+              <ul>
+                <li>
+                  Frontend
+                  <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                  </ul>
+                </li>
+                <li>Backend</li>
+              </ul>
+            </li>
+          </ul>
+        </section>
+      )}
     </section>
-    );
-  }
-  
-  export default Skills;
+  );
+}
+
+export default Skills;
